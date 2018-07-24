748999bfe7e9737630206d9672b7c5fc6f9532a4
Kevin Somao <kevin@startupfactory.com>
2018-07-24 14:31:10 +0200

    Activity component created

---
+++ src/app/components/home_page/Activity.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Loading} from 'element-react'
+
+const Activity = ({activity}) => {
+    return (
+        <div className={"activity w-75 d-flex flex-column justify-content-center text-center mb-4"}>
+            <div className={"img-container mb-1"}>
+                <img src={activity.icon} className={"responsive mb-4"}/>
+            </div>
+            <h3 className={"mb-3"}>{activity.title}</h3>
+            <p>{activity.text}</p>
+        </div>
+    )
+}
+
+
+export default Activity
--- src/app/screens/Home.js
+++ src/app/screens/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import {connect} from 'react-redux'
-
 import ButtonImg from '../components/home_page/ButtonImg'
 import Menu from '../components/home_page/Menu'
+import content from '../content'
 
 import {loadBirds} from '../store/birds/actions'
 import SectionTitle from "../components/home_page/SectionTitle";
+import Activity from "../components/home_page/Activity";
 
 
 class Home extends React.Component {
@@ -13,20 +14,31 @@ class Home extends React.Component {
         super(props)
         this.state = {
             isRefreshing: false,
+            data: []
         }
     }
 
     componentDidMount() {
         this.props.dispatch(loadBirds())
+        this.setState({data: JSON.parse(JSON.stringify(content))})
     }
 
     render() {
-        return (
-            <div>
-                <Menu/>
-                <SectionTitle text={"Nous"}/>
-            </div>
+        if (!this.state.data.activities) {
+            return <div/>
+        }
 
+        return (
+            <div className={"container"}>
+                <Menu/>
+                <SectionTitle text={"Alentours"}/>
+
+                <div className="activities mt-5 d-flex flex-column align-items-center ">
+                    {this.state.data.activities.map((activity, index) => (
+                        <Activity activity={activity} key={index}/>
+                    ))}
+                </div>
+            </div>
         )
     }
 }
