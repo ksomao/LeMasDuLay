240d6b2aa5efb0daa7287be5c3ac3654d3b6bfca
Kevin Somao <kevin@startupfactory.com>
2018-07-25 10:09:29 +0200

    commit before pull

--- src/app/screens/Home.js
+++ src/app/screens/Home.js
@@ -38,9 +38,9 @@ class Home extends React.Component {
             <div className={"Alentours"}>
                 <SectionTitle text={"Alentours"}/>
                 <div className="activities mt-5 d-flex flex-column align-items-center ">
-                    return {this.state.data.activities.map((activity, index) => (
-                    <Activity activity={activity} key={index}/>
-                ))}
+                    {this.state.data.activities.map((activity, index) => (
+                        <Activity activity={activity} key={index}/>
+                    ))}
                 </div>
             </div>
         )
