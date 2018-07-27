89e771df7265daae4331d1510fdfa3e0559915d7
Kevin Somao <kevin@startupfactory.com>
2018-07-27 19:25:02 +0200

    merge before pull

--- src/app/screens/App.js
+++ src/app/screens/App.js
@@ -1,19 +1,18 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 import Booking from './Booking'
 import Home from "./Home";
 
 const App = props => {
-	return (
-		<Router>
-			<Switch>
-				<Route
-					path='*'
-					component={Home} />
-			</Switch>
-		</Router>
-	)
+    return (
+        <Router>
+            <Switch>
+                <Route path='/Booking' component={Booking}/>
+                <Route path='/' component={Home}/>
+            </Switch>
+        </Router>
+    )
 }
 
 export default App
